f8eebc963dd898ba30978b76d580a836eb2bed98
chenxiaotao <chenxiaotao.nomad@bigo.sg>
2025-05-20 10:45:02 +0800

    feat: 配置

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: './',
   build: {
     chunkSizeWarningLimit: 600, // 调整大小警告限制到600kb
+    assetsDir: 'assets', // 指定资源目录名称
   }
 })
